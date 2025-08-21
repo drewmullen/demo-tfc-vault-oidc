6e66ef5a61ec4f06f205d4992b9d5bb2a0b1d539
Drew Mullen <drew.mullen@hashicorp.com>
2025-08-21 16:21:23 -0400

    dynamic not working yet

--- setup-directoidc/impersonation/workspace_dynamic/main.tf
+++ setup-directoidc/impersonation/workspace_dynamic/main.tf
@@ -10,18 +10,47 @@ terraform {
 }
 
 provider "google" {
-  project = var.gcp_project_id
-  access_token = data.google_service_account_access_token.impersonated_access_token.access_token
+  credentials = var.tfc_gcp_dynamic_credentials.default.credentials
+}
+
+provider "google" {
+  alias = "ALIAS1"
+  credentials = var.tfc_gcp_dynamic_credentials.default.credentials
+  #var.tfc_gcp_dynamic_credentials.aliases["ALIAS1"].credentials
   impersonate_service_account = "tfc-sa@tfc-oidc-464517.iam.gserviceaccount.com"
 }
 
-# Retrieve client configuration (includes project, region, access token, etc.)
-data "google_client_config" "current" {}
-
-# Output the project ID and access token
-output "current_project" {
-  value = data.google_client_config.current.project
+variable "tfc_gcp_dynamic_credentials" {
+  description = "Object containing GCP dynamic credentials configuration"
+  type = object({
+    default = object({
+      credentials = string
+    })
+    aliases = map(object({
+      credentials = string
+    }))
+  })
 }
+
+
+
+
+
+# provider "google" {
+#   project = var.gcp_project_id
+#   access_token = data.google_service_account_access_token.impersonated_access_token.access_token
+#   impersonate_service_account = "tfc-sa@tfc-oidc-464517.iam.gserviceaccount.com"
+# }
+
+# Retrieve client configuration (includes project, region, access token, etc.)
+data "google_client_config" "current" {
+  provider = google.ALIAS1
+}
+
+# # Output the project ID and access token
+# output "current_project" {
+#   value = data.google_client_config.current.project
+# }
 
 
 # data "google_service_account" "account_to_impersonate" {
@@ -36,12 +65,12 @@ output "current_project" {
 # }
 
 # # Data source to fetch access token for impersonation
-data "google_service_account_access_token" "impersonated_access_token" {
-#   provider               = google
-  target_service_account = data.google_service_account.account_to_impersonate.email
-  scopes                 = ["https://www.googleapis.com/auth/cloud-platform"]
-  lifetime               = "600s"
-}
+# data "google_service_account_access_token" "impersonated_access_token" {
+# #   provider               = google
+#   target_service_account = data.google_service_account.account_to_impersonate.email
+#   scopes                 = ["https://www.googleapis.com/auth/cloud-platform"]
+#   lifetime               = "600s"
+# }
 
 # data "google_service_account" "impersonated_check" {
 #   provider = google.impersonated
